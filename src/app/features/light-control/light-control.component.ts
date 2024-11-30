cee461a5aa02c1f07a7f8cd327a097d17228f605
SammAPH <samalepuhu@gmail.com>
2024-11-29 19:37:13 -0500

    light-control component creation and upload

---
+++ src/app/features/light-control/light-control.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-light-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './light-control.component.html',
+  styleUrl: './light-control.component.scss'
+})
+export class LightControlComponent {
+
+}
